a42403bacbe2b12aeb8e4fcd24adef4ef6df0ee1
GichukiPaul <gichukipaul043@gmail.com>
2025-01-10 12:14:17 +0300

    Initial Commit

---
+++ DVT Weather AppTests/DVT_Weather_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DVT_Weather_AppTests.swift
+//  DVT Weather AppTests
+//
+//  Created by GICHUKI on 10/01/2025.
+//
+
+import Testing
+@testable import DVT_Weather_App
+
+struct DVT_Weather_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
